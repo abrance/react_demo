88b1b602132969d5c181c65d1c5d539aa2670d52
abrance <your_email>
2021-05-06 17:02:28 +0800

    remove some useless file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-// import logo from './logo.svg';
 import './App.css';
 
 import React from "react";
-// import * as assert from "assert";
 import {FooterComponent, HeaderComponent, SectionComponent} from "./cpts";
 
 
